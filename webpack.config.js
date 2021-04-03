a23529152d8bb6e05fa6f98b2717e1ff51d8141b
Sumit H <ssumitofficial@gmail.com>
2021-04-03 21:38:04 +0530

    React boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node-modules/,
+                use: '@babel/preset-react',
+            }
+        ]
+    }
+}
